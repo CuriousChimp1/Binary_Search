24fc2f8073104af60e8196e6a5f4113d74fc3e62
CuriousChimp1 <curiouschimp100@gmail.com>
2022-10-26 11:43:29 +0530

    Ceiling and Flooring of a number using Binary Search.

---
+++ src/Ceiling.java
@@ -0,0 +1,42 @@
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] a = {-1, 0, 1, 4, 6, 8, 90};
+        int t = 19;
+        int ans = ceiling(a, t);
+        int ans1 = flooring(a, t);
+        System.out.println(ans);
+        System.out.println(ans1);
+    }
+
+    static int ceiling(int[] a, int t){
+        int s = 0;
+        int e = a.length - 1;
+        while(s <= e){
+            int m = (s + e) / 2;
+
+            if(a[m] < t)
+                s = m + 1;
+            else if(a[m] > t)
+                e = m - 1;
+            else
+                return a[m];
+        }
+        return a[s];
+    }
+
+    static int flooring(int[] a, int t){
+        int s = 0;
+        int e = a.length - 1;
+        while(s <= e){
+            int m = (s + e) / 2;
+
+            if(a[m] < t)
+                s = m + 1;
+            else if(a[m] > t)
+                e = m - 1;
+            else
+                return a[m];
+        }
+        return a[e];
+    }
+}
